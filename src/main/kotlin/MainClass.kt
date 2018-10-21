a2fb0ef36eb301d5f47849f47c219d4674104986
Balram Pandey <balrampandey@Balrams-MacBook-Air.local>
2018-10-22 00:19:18 +0530

    routing

--- src/main/kotlin/MainClass.kt
+++ src/main/kotlin/MainClass.kt
@@ -1,5 +1,18 @@
+import io.ktor.http.ContentType
+import io.ktor.http.cio.websocket.FrameType.Companion.get
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+fun main(args: ArrayList<String>) {
 
-fun main(args:ArrayList<String>)
-{
-
+    embeddedServer(Netty, 8080)
+    {
+        routing {
+           get("/")
+           {
+               respo
+           }
+        }
+    }
 }
